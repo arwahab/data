d3770fb59562cecba120d7be25724ee513ae2c2e
Abdul Wahab <arwahab@users.noreply.github.com>
2021-06-13 23:41:27 -0400

    Delete recommendation_engine.py

--- python/recommendation_engine.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# loading movies metadata
-metadata = pd.read_csv('movies_metadata.csv', low_memory=False)
-
-# print the first three rows to a source file
-source_file = open('output.txt', 'w')
-print(metadata.head(3), file=source_file)
-source_file.close()
